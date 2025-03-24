69f6687b28294d1f78ebc55325e8831de3748fb8
ljnnna <heymenania@gmail.com>
2025-03-24 19:16:42 +0700

    pweb nania payment dan history (masi error)

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,19 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\HomeController;
+//use App\Http\Controllers\HomeController;
 
 // Route::get('/', function () {
 //     return view('welcome');
 // });
 
-Route::get('/', [HomeController::class, 'index']);
-Route::get('/contact', [HomeController::class, 'contact']);
+//Route::get('/', [HomeController::class, 'index']);
+//Route::get('/contact', [HomeController::class, 'contact']);
 
+Route::get('/', function () {
+    return view('payment');
+});
+
+Route::get('/', function () {
+    return view('history');
+});
